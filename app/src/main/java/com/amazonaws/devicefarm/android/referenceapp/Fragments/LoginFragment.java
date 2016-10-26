52a9300efe32b271528fe0bd3140d2664d2118ae
Roma Mulchandani <rmulchandani@digite.in>
2016-10-26 19:09:29 +0530

    Modifying for test

--- app/src/main/java/com/amazonaws/devicefarm/android/referenceapp/Fragments/LoginFragment.java
+++ app/src/main/java/com/amazonaws/devicefarm/android/referenceapp/Fragments/LoginFragment.java
@@ -12,6 +12,8 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+ 
+
 package com.amazonaws.devicefarm.android.referenceapp.Fragments;
 
 import android.os.Bundle;
